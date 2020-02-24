3169bbb3ab07584526b9466bc81c910bf27038cb
balajim <m.balajee@yahoo.com>
2020-02-24 09:55:31 -0500

    Intitial commit

---
+++ app/src/main/java/com/apps/firebasechat/User.java
@@ -0,0 +1,25 @@
+package com.apps.firebasechat;
+
+public class User {
+    private String name;
+    private String email;
+    private String uid;
+
+    public User(String name, String email, String uid) {
+        this.name = name;
+        this.email = email;
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+}
